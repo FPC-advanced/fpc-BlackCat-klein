efcf9dfcfeab2ec47b9029e2f08c334c2d9f1f11
BlackCat-klein <k.santhosh7612@gmail.com>
2023-12-19 13:26:47 +0530

    committed 12/19/2023, 1:26:47 PM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,8 +1,10 @@
 // 1. Write a C program to print your name.
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     char s;
     printf("Santhosh \n");
+    printf("X");
     return 0;
 }
